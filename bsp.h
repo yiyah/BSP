75f48de570cdd3bfef96a16116208b62bc91663f
yiyah <756307810@qq.com>
2024-01-30 10:04:16 +0800

    :sparkles: [feat] add printf support

--- bsp.h
+++ bsp.h
@@ -1,6 +1,8 @@
 #ifndef __BSP_H__
 #define __BSP_H__
 
+#include "types.h"
 #include "led.h"
+#include "iic.h"
 
 #endif
--- types.h
+++ types.h
@@ -1,6 +1,11 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+/**
+ * @brief typedef data type
+ * 
+ * @note  work with 32bit platform
+ */
 typedef unsigned char           u8;
 typedef unsigned short int      u16;
 typedef unsigned int            u32;
